c314bfc4e7787d326ae17289d77400ab1e62e4ab
Farid Chavez <faridchavezcampos@gmail.com>
2026-09-01 23:41:47 -0500

    Modifico StudentController en sprint-2

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -1,4 +1,9 @@
 package pe.edu.tecsup.lab03.controllers;
 
 public class StudentController {
+
+    public void mostrarMensaje() {
+        System.out.println("StudentController actualizado en sprint-2");
+    }
+
 }
